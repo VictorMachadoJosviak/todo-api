eca4867f6a7d39b0fcc7dc5125ababa5a42d2384
VictorMachadoJosviak <victormjosviak@gmail.com>
2022-01-10 23:19:05 -0300

    add app controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { ConfigurationModule } from './configuration/configuration.module';
 import { DatabaseModule } from './database/database.module';
 import { TodoModule } from './todo/todo.module';
 import { HealthcheckModule } from './healthcheck/healthcheck.module';
+import { AppController } from './app/app.controller';
 
 @Module({
   imports: [ConfigurationModule, DatabaseModule, TodoModule, HealthcheckModule],
-  controllers: [],
+  controllers: [AppController],
   providers: [],
 })
 export class AppModule {}
